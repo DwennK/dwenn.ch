e6de0711c58dae98e1e926edd67d0d4aa63cb5cb
DwennK <dwenn@live.fr>
2024-02-19 22:21:16 +0100

    modify headline

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Let's build a boilerplate</h1>
+      <h1>
+        I established{" "}
+        <Link
+          className="font-bold hover:text-blue-600"
+          href="https://peaks.ch/"
+        >
+          Peaks.ch
+        </Link>{" "}
+        with the aim of empowering small businesses to enhance their online
+        presence.
+      </h1>
     </main>
   );
 }
