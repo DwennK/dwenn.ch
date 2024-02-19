11c524ee37f577a4ee8d22980d6737e3171227b2
DwennK <dwenn@live.fr>
2024-02-19 23:50:23 +0100

    Modify Herosection

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        I established{" "}
-        <Link
-          className="font-bold hover:text-blue-600"
-          href="https://peaks.ch/"
-        >
-          Peaks.ch
-        </Link>{" "}
-        with the aim of empowering small businesses to enhance their online
-        presence.
-      </h1>
+    <main className="flex min-h-screen flex-col items-center container p-24">
+      <HeroSection />
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        I'm a Web developper based in Switzerland
+      </h2>
     </main>
   );
 }
---
+++ components/HeroSection.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Download } from "lucide-react";
+import { Contact } from "lucide-react";
+
+export const HeroSection = () => {
+  return (
+    <section>
+      {" "}
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Hello, I'm Dwenn
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        I established{" "}
+        <Link
+          className="font-bold hover:text-blue-600"
+          href="https://peaks.ch/"
+        >
+          Peaks.ch
+        </Link>{" "}
+        with the aim of empowering small businesses to enhance their online
+        presence.
+      </p>
+      <div className="mt-4 mb-4">
+        {" "}
+        <Button className="mr-4">
+          <Contact className="mr-2 h-4 w-4" />
+          Hire me
+        </Button>
+        <Button variant="outline" className="mr-4">
+          <Download className="mr-2 h-4 w-4" />
+          Download CV
+        </Button>
+      </div>
+      <div className="col-span-5 mt-4 mb-4">
+        <div className="rounded-full bg-slate-200 dark:bg-slate-900 flex items-center justify-center relative">
+          <Image
+            src="/images/hero-image.png"
+            alt="hero image"
+            width={300}
+            height={300}
+          ></Image>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
