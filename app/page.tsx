7a038a90627c1a9108aa2c534d9eb66bf99b1003
DwennK <dwenn@live.fr>
2024-02-21 22:00:34 +0100

    fix padding main section page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center container p-24">
+    <main className="flex min-h-screen flex-col items-center container p-16 m:p-24">
       <HeroSection />
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         I am a Web developper based in Switzerland
